97c5286857ff4ae7d90828ca3c5fc77dcf635dc2
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 04:56:27 +0000

    Auto-format by https://ultralytics.com/actions

--- scan.py
+++ scan.py
@@ -1,9 +1,9 @@
 import cv2
+
 from ultralytics import YOLO
 
 # 加载 YOLOv8 模型
 model = YOLO(r"E:\Python\yolov8\ultralytics-main\runs\detect\face_detection27\weights\best.pt")
-
 
 
 cap = cv2.VideoCapture(0)
@@ -16,9 +16,9 @@ while True:
     results = model.predict(frame)
     # 在当前帧图像上绘制人脸边界框等信息方便查看
     annotated_frame = results[0].plot()
-    cv2.imshow('Face Detection', annotated_frame)
+    cv2.imshow("Face Detection", annotated_frame)
 
-    if cv2.waitKey(1) == ord('q'):
+    if cv2.waitKey(1) == ord("q"):
         break
 
 cap.release()
