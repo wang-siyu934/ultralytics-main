a2843777462f8fdf7ec0b67352ddb05c99c35af1
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 04:56:24 +0000

    Auto-format by https://ultralytics.com/actions

--- face.py
+++ face.py
@@ -8,9 +8,9 @@ data_yaml_path = r"E:\Python\yolov8\ultralytics-main\widerface\data.yaml"
 # 开始训练
 results = model.train(
     data=data_yaml_path,  # 数据集配置文件
-    epochs=10,            # 训练轮数
-    imgsz=640,            # 输入图像大小
-    batch=16,             # 批量大小
-    device="cpu",         # 使用的设备（CPU 或 GPU）
-    name="face_detection" # 训练结果保存的文件夹名称
+    epochs=10,  # 训练轮数
+    imgsz=640,  # 输入图像大小
+    batch=16,  # 批量大小
+    device="cpu",  # 使用的设备（CPU 或 GPU）
+    name="face_detection",  # 训练结果保存的文件夹名称
 )
